e6076e75010cbfdc03b243328b2992c185cd844f
imtiyaz-78 <imtiyazmd803@gmail.com>
2026-08-23 00:05:25 +0530

    Generic Tost Implemented

--- rfq_ui_project/src/app/features/client/dashboard/dashboard.ts
+++ rfq_ui_project/src/app/features/client/dashboard/dashboard.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ToastService } from '../../../core/services/toast.service';
 
 @Component({
   selector: 'app-client-dashboard',
@@ -9,4 +10,16 @@ import { RouterModule } from '@angular/router';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class ClientDashboardComponent {}
+export class ClientDashboardComponent implements OnInit {
+  toastService = inject(ToastService);
+
+  ngOnInit(): void {
+    // Trigger Success notification for testing
+    this.toastService.showToast({
+      type: 'success',
+      title: 'Welcome Back!',
+      message: 'Logged in successfully as Rahul Sharma.'
+    });
+
+  }
+}
